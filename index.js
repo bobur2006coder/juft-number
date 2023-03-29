64f9d2384c4ca6d448c20d78e1be601744062bc2
bobur2006coder <boburdilmurodov702@gmail.com>
2023-03-29 11:03:57 +0500

    origin

---
+++ index.js
@@ -0,0 +1,11 @@
+let a = prompt("sonni kiriting");
+
+if(a <= -0){
+ alert("siz xato son kiritingiz")
+}else if(a%2===0){
+    alert("siz juft son kiritdingiz")
+}else if(a%2===1){
+    alert("siz toq son kirtdingiz")
+}else{
+    alert("mavjud bo`lmagan qiymat")
+}
